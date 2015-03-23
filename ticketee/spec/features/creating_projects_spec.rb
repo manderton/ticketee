14d99cd834b0f7ec13c4f6573bf48d371e05ced8
Matthew Anderton <matt.anderton@plansource.com>
2015-03-23 11:27:37 -0600

    project validation

--- ticketee/spec/features/creating_projects_spec.rb
+++ ticketee/spec/features/creating_projects_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.feature "Creating Projects" do
 
-  scenario 'A user can create a new project' do
+  before do
     visit "/"
-
     click_link "New Project"
+  end
+
+  scenario 'A user can create a new project' do
 
     fill_in "Name", with: "Sublime Text 3"
     fill_in "Description", with: "A text editor for everyone"
@@ -20,5 +22,13 @@ RSpec.feature "Creating Projects" do
     title = "Sublime Text 3 - Projects - Ticketee"
 
     expect(page).to have_title(title)
+  end
+
+  scenario 'A user cannot create a project without a name' do
+
+    click_button "Create Project"
+
+    expect(page).to have_content("Project has not been created.")
+    expect(page).to have_content("Name can't be blank")
   end
 end
