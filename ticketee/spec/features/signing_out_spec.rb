f82cdf9cb9138cbe01e6b82af3eb6430f4d2b369
Matthew Anderton <matt.anderton@plansource.com>
2015-03-24 16:04:09 -0600

    add sign in and sign out

---
+++ ticketee/spec/features/signing_out_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.feature "A signed in user can sign out" do
+  let!(:user) { FactoryGirl.create(:user) }
+
+  before do
+    login_as(user)
+  end
+
+  scenario do
+    visit "/"
+
+    click_link "Signed in as #{user.email}"
+    expect(page).to have_content("Signed out successfully.")
+  end
+end
